d93e8b04a1423603f24ed4d7d1119ac667af8536
vinita poonia <vinitapoonia333@gmail.com>
2024-01-05 05:09:34 +0530

    routes of frontend, defined hooks for fetching user

---
+++ client/src/App.js
@@ -0,0 +1,29 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./App.css";
+import { Navbar } from "./components/navbar";
+import { Auth } from "./pages/auth";
+import { CreateRecipe } from "./pages/create-recipe";
+import { Home } from "./pages/home";
+import { SavedRecipes } from "./pages/saved-recipes";
+import RecipeDetails from "./pages/recipe-details";
+import { EditRecipe } from "./pages/edit-recipe";
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/create-recipe" element={<CreateRecipe />} />
+          <Route path="/saved-recipes" element={<SavedRecipes />} />
+          <Route path="/auth" element={<Auth />} />
+          <Route path="/recipes/:id" element={<RecipeDetails/>} />
+          <Route path="/recipes/edit/:id" element={<EditRecipe/>} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
